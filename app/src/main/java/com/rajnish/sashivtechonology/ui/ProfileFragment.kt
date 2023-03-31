ad210e61035aa140ea846a23bc848871b68b270c
Rajnish <kumarrajnish.stm@gmial.com>
2023-03-31 17:58:26 +0530

    edit profile page work done by 4 to 5 batch

--- app/src/main/java/com/rajnish/sashivtechonology/ui/ProfileFragment.kt
+++ app/src/main/java/com/rajnish/sashivtechonology/ui/ProfileFragment.kt
@@ -5,11 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.rajnish.sashivtechonology.R
+import com.rajnish.sashivtechonology.databinding.FragmentProfileBinding
 
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    private lateinit var binding: FragmentProfileBinding
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding = FragmentProfileBinding.bind(view)
         super.onViewCreated(view, savedInstanceState)
+        val navController = findNavController()
+
+        binding.imgPic.setOnClickListener{
+            val action = ProfileFragmentDirections.actionProfileFragmentToEditProfileFragment()
+            navController.navigate(action)
+        }
     }
 }
