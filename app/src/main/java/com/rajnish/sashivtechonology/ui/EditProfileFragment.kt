dea8bef69af99261f538908d4f3291139d5504da
Rajnish <kumarrajnish.stm@gmial.com>
2023-03-31 18:01:12 +0530

    new toast added

--- app/src/main/java/com/rajnish/sashivtechonology/ui/EditProfileFragment.kt
+++ app/src/main/java/com/rajnish/sashivtechonology/ui/EditProfileFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.rajnish.sashivtechonology.R
 
 
@@ -12,6 +13,7 @@ class EditProfileFragment : Fragment(R.layout.fragment_edit_profile) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Toast.makeText(context, "Check incoming branch", Toast.LENGTH_SHORT).show()
     }
 
 }
