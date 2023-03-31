e76cfb298b0027b947043882a88007927861d6f5
Rajnish <kumarrajnish.stm@gmial.com>
2023-03-31 09:07:49 +0530

    nav work done

--- app/src/main/java/com/rajnish/sashivtechonology/MainActivity.kt
+++ app/src/main/java/com/rajnish/sashivtechonology/MainActivity.kt
@@ -2,17 +2,39 @@ package com.rajnish.sashivtechonology
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+
+import com.rajnish.sashivtechonology.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
+   val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
 
-        Toast.makeText(this, "HI this is from sashiv", Toast.LENGTH_SHORT).show()
-        Toast.makeText(this, "HI this is from sashiv", Toast.LENGTH_SHORT).show()
+      appBarConfiguration = AppBarConfiguration(
+          setOf(R.id.homeFragment,R.id.dashBoardFragment,R.id.profileFragment),
+          binding.drawerLayout
+      )
 
+      binding.toolbar.setupWithNavController(navController,appBarConfiguration)
+        binding.buttonNavView.setupWithNavController(navController)
+        binding.navDrawer.setupWithNavController(navController)
 
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/rajnish/sashivtechonology/ui/ProfileFragment.kt
@@ -0,0 +1,15 @@
+package com.rajnish.sashivtechonology.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rajnish.sashivtechonology.R
+
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
